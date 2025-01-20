578696d85b25ffa2221b82e1082844252728a607
David Gomez G. <dgomezg@gmail.com>
2025-01-20 18:23:42 +0100

    Solved Exercism/assembly-line

--- exercism/assembly-line/assembly_line.rb
+++ exercism/assembly-line/assembly_line.rb
@@ -1,13 +1,22 @@
 class AssemblyLine
+  CARS_PER_HOUR = 221
+
   def initialize(speed)
     @speed = speed
+    @sucess_rate = case speed
+                   when 1..4  then 1
+                   when 5..8  then 0.9
+                   when 9     then 0.8
+                   when 10    then 0.77
+                   else 0
+                   end
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    @speed * CARS_PER_HOUR * @sucess_rate
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).to_i
   end
 end
