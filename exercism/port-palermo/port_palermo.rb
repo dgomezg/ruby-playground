4941e33460624590a77ce13dfb047e4a23dc0551
David Gomez G. <dgomezg@gmail.com>
2025-01-21 11:00:08 +0100

    Solved Exercism/port-palermo problem

--- exercism/port-palermo/port_palermo.rb
+++ exercism/port-palermo/port_palermo.rb
@@ -1,11 +1,15 @@
 module Port
-  # TODO: define the 'IDENTIFIER' constant
+  # DONE: define the 'IDENTIFIER' constant
+  IDENTIFIER = :PALE
 
   def self.get_identifier(city)
-    raise 'Please implement the Port.get_identifier method'
+    city[0..3].upcase.to_sym
   end
 
   def self.get_terminal(ship_identifier)
-    raise 'Please implement the Port.get_terminal method'
+    case ship_identifier.to_s[0...3].upcase.to_sym
+    when :OIL, :GAS then :A
+    else :B
+    end
   end
 end
