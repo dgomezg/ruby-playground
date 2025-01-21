2a185458379a1c82cb86d2a952644b44235d57b1
David Gomez G. <dgomezg@gmail.com>
2025-01-21 11:26:24 +0100

    Solved Exercism/chess-game problem

--- exercism/chess-game/chess_game.rb
+++ exercism/chess-game/chess_game.rb
@@ -1,16 +1,22 @@
 module Chess
-  # TODO: define the 'RANKS' constant
-  # TODO: define the 'FILES' constant
+  # DONE: define the 'RANKS' constant
+  RANKS = 1..8
+  # DONE: define the 'FILES' constant
+  FILES = 'A'..'H'
 
   def self.valid_square?(rank, file)
-    raise "Please implement the Chess.valid_square? method"
+    RANKS.include? rank and FILES.include? file
   end
 
   def self.nick_name(first_name, last_name)
-    raise "Please implement the Chess.nick_name method"
+    "#{first_name[0..1].upcase}#{last_name[-2..-1].upcase}"
   end
 
   def self.move_message(first_name, last_name, square)
-    raise "Please implement the Chess.move_message method"
+    if valid_square?(square[1].to_i, square[0])
+      "#{nick_name(first_name, last_name)} moved to #{square}"
+    else
+      "#{nick_name(first_name, last_name)} attempted to move to #{square}, but that is not a valid move"
+    end
   end
 end
