adc0efbe00b846adc752c7dd37fcd3861213a22a
David Gomez G. <dgomezg@gmail.com>
2025-01-22 21:08:42 +0100

    Solved Exercism/simple-calculator

--- exercism/simple-calculator/simple_calculator.rb
+++ exercism/simple-calculator/simple_calculator.rb
@@ -1,7 +1,20 @@
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
+  class UnsupportedOperation < StandardError; end
+
   def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+    unless ALLOWED_OPERATIONS.include?(operation)
+      raise UnsupportedOperation.new("Operation not supported: #{operation}")
+    end
+    unless first_operand.is_a?(Numeric) && second_operand.is_a?(Numeric)
+      raise ArgumentError.new("Operands #{[first_operand, second_operand]} must be Numeric")
+    end
+    begin
+      result = eval "#{first_operand} #{operation} #{second_operand}"
+      "#{first_operand} #{operation} #{second_operand} = #{result}"
+    rescue ZeroDivisionError
+      "Division by zero is not allowed."
+    end
   end
 end
