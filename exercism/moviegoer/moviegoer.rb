304cec9032d05aaaad904a62b8eaf1e843736814
David Gomez G. <dgomezg@gmail.com>
2025-01-22 21:26:39 +0100

    Solved Exercism/moviegoer

--- exercism/moviegoer/moviegoer.rb
+++ exercism/moviegoer/moviegoer.rb
@@ -3,21 +3,23 @@ class NotMovieClubMemberError < RuntimeError
 end
 
 class Moviegoer
+
   def initialize(age, member: false)
     @age = age
     @member = member
   end
 
   def ticket_price
-    raise 'Please implement the Moviegoer#ticket_price method'
+    @age >= 60? 10 : 15
   end
 
   def watch_scary_movie?
-    raise 'Please implement the Moviegoer#watch_scary_movie method'
+    @age >= 18
   end
 
   # Popcorn is 🍿
   def claim_free_popcorn!
-    raise 'Please implement the Moviegoer#claim_free_popcorn method'
+    raise NotMovieClubMemberError unless @member
+     "🍿"
   end
 end
