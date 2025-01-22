821ffe16165939f5af9a846a6c53fa362f79ccd5
David Gomez G. <dgomezg@gmail.com>
2025-01-22 20:21:39 +0100

    Solved Exercism/Locomotive-engineer

--- exercism/locomotive-engineer/locomotive_engineer.rb
+++ exercism/locomotive-engineer/locomotive_engineer.rb
@@ -1,17 +1,18 @@
 class LocomotiveEngineer
-  def self.generate_list_of_wagons
-    raise 'Please implement the LocomotiveEngineer.generate_list_of_wagons method'
+  def self.generate_list_of_wagons(*wagons)
+    wagons
   end
 
   def self.fix_list_of_wagons(each_wagons_id, missing_wagons)
-    raise 'Please implement the LocomotiveEngineer.fix_list_of_wagons method'
+    (a, b, locomotive, *tail) = each_wagons_id
+    [locomotive, *missing_wagons, *tail, a, b]
   end
 
-  def self.add_missing_stops
-    raise 'Please implement the LocomotiveEngineer.add_missing_stops method'
+  def self.add_missing_stops(route , **stops)
+    {**route, stops: stops.values}
   end
 
   def self.extend_route_information(route, more_route_information)
-    raise 'Please implement the LocomotiveEngineer.extend_route_information method'
+    {**route, **more_route_information}
   end
 end
