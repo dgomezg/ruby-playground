45c231009451e6ec0ea083500d0824b00becc75f
David Gomez G. <dgomezg@gmail.com>
2025-01-23 18:35:32 +0100

    Solved Exercism/dnd-character practice

--- exercism/dnd-character/dnd_character.rb
+++ exercism/dnd-character/dnd_character.rb
@@ -1,17 +1,26 @@
-=begin
-Write your code for the 'D&D Character' exercise in this file. Make the tests in
-`dnd_character_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/dnd-character` directory.
-=end
 
 class DndCharacter
-  def self.modifier
-    # Your code here
+  @skills
+  @hitpoints
+  attr_reader :hitpoints
+  def self.modifier(constitution)
+    (constitution-10) / 2
   end
 
   def initialize
-    # Your code here
+    @skills = %i[strength dexterity constitution intelligence wisdom charisma].zip(Array.new(6,0)).to_h
+    @skills.keys.each do |skill|
+      dices = Array.new(4) {|_| rand(1..6)}
+      @skills[skill] = dices.max(3).sum
+    end
+    @hitpoints = 10 + DndCharacter.modifier(@skills[:constitution])
+  end
+
+  private def method_missing(symbol, *args)
+    if @skills.keys.include?(symbol)
+      @skills[symbol]
+    else
+      super.send(symbol, *args)
+    end
   end
 end
