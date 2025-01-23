2c75da098e653bc599f1bae3eecf9288305ccd87
David Gomez G. <dgomezg@gmail.com>
2025-01-23 16:20:57 +0100

    Improved Exercism/resistor-color-duo based on community solutions

--- exercism/resistor-color-duo/resistor_color_duo.rb
+++ exercism/resistor-color-duo/resistor_color_duo.rb
@@ -1,8 +1,13 @@
 module ResistorColorDuo
-  RESISTOR_COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"].freeze
+  #RESISTOR_COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"].freeze
+  # Optimization made after seeing community solutions.
+  # %w(...) is a shortcut in Ruby to create an array of Strings without having to type quotes and commas...
+  RESISTOR_COLORS = %w(black brown red orange yellow green blue violet grey white)
 
   def self.value(colors)
-    colors.map{|color| RESISTOR_COLORS.index(color).to_s}[0..1].join.to_i
+    #colors.map{|color| RESISTOR_COLORS.index(color).to_s}[0..1].join.to_i
+    # Optimization made after seeing community solutions.
+    colors.take(2).map{ |color| RESISTOR_COLORS.index(color).to_s}.join.to_i
   end
 
 end
