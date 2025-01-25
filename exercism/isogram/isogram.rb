b87c17735b5fbc5608e51b0c41f3ed26aee89674
David Gomez G. <dgomezg@gmail.com>
2025-01-25 15:02:51 +0100

    Added alternative solution for Exercism/isogram after checking community solutions

--- exercism/isogram/isogram.rb
+++ exercism/isogram/isogram.rb
@@ -1,9 +1,13 @@
 module Isogram
   def self.isogram?(string)
-    string.gsub(/[^0-9A-Za-z]/, '')
-          .downcase
-          .chars
-          .group_by{|e| e}
-          .all?{|_, v| v.length == 1}
+    #string.gsub(/[^0-9A-Za-z]/, '')
+    #      .downcase
+    #      .chars
+    #      .group_by{|e| e}
+    #      .all?{|_, v| v.length == 1}
+
+    #Alternative solution after checking community solutions
+    letters = string.downcase.scan(/\w/)
+    letters.count === letters.uniq.count
   end
 end
