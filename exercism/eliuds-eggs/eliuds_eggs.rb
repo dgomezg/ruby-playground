8e9021c7ebfa49712576ecc584ede6be1f362398
David Gomez G. <dgomezg@gmail.com>
2025-01-25 13:38:45 +0100

    Solved Exercism/EliudEggs

--- exercism/eliuds-eggs/eliuds_eggs.rb
+++ exercism/eliuds-eggs/eliuds_eggs.rb
@@ -1,7 +1,5 @@
-=begin
-Write your code for the 'Eliuds Eggs' exercise in this file. Make the tests in
-`eliuds_eggs_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/eliuds-eggs` directory.
-=end
+module EliudsEggs
+  def self.egg_count(display_number)
+    display_number.to_s(2).count('1')
+  end
+end
