faf40a88f2b9dc2f67cdd027280d0b3110c02593
David Gomez G. <dgomezg@gmail.com>
2025-01-25 11:36:44 +0100

    Added Exercism/series practice

---
+++ exercism/series/.meta/example.rb
@@ -0,0 +1,18 @@
+class Series
+  def initialize(series)
+    raise ArgumentError if series.length.zero?
+    @series = series
+  end
+  def slices(n)
+    if n > @series.length || n <= 0
+      raise ArgumentError
+    end
+    series = @series
+    substrings = []
+    until series.length < n
+      substrings << series[0..n-1]
+      series = series[1..-1]
+    end
+    substrings
+  end
+end
