1a452a247cce82ccf47a6386250efa6769c2348c
David Gomez G. <dgomezg@gmail.com>
2025-01-25 14:14:46 +0100

    Improved Exercism/raindrop based on community solutions

--- exercism/raindrops/raindrops.rb
+++ exercism/raindrops/raindrops.rb
@@ -4,6 +4,6 @@ module Raindrops
     result += 'Pling' if (num % 3).zero?
     result += 'Plang' if (num % 5).zero?
     result += 'Plong' if (num % 7).zero?
-    result.length > 0? result : num.to_s
+    result.empty? ? num.to_s : result
   end
 end
