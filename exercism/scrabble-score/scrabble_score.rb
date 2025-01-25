7fdc37ec9a44742ceb185b5f46fe76defe489ae9
David Gomez G. <dgomezg@gmail.com>
2025-01-25 15:30:31 +0100

    Solved Exercism/scrable-score practice

--- exercism/scrabble-score/scrabble_score.rb
+++ exercism/scrabble-score/scrabble_score.rb
@@ -1,7 +1,22 @@
-=begin
-Write your code for the 'Scrabble Score' exercise in this file. Make the tests in
-`scrabble_score_test.rb` pass.
+class Scrabble
+  attr_reader :score
+  def initialize(word)
+    @score = word.upcase.scan(/\w/).map do |char|
+      char_score(char)
+    end.sum
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/scrabble-score` directory.
-=end
+  private def char_score(char)
+  case char
+  when *%w(A E I O U L N R S T) then 1
+  when *%w(D G) then 2
+  when *%w(B C M P) then 3
+  when *%w(F H V W Y) then 4
+  when *%w(K) then 5
+  when *%w(J X) then 8
+  when *%w(Q Z) then 10
+  else 0
+  end
+
+  end
+end
