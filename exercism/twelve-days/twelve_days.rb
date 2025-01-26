c0410547e47c1651241d746b161d49051ce58da0
David Gomez G. <dgomezg@gmail.com>
2025-01-26 18:33:16 +0100

    Solved Exercism/twelve-days

--- exercism/twelve-days/twelve_days.rb
+++ exercism/twelve-days/twelve_days.rb
@@ -1,7 +1,33 @@
-=begin
-Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-`twelve_days_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/twelve-days` directory.
-=end
+module TwelveDays
+  TRUE_LOVE = [
+          ["first", "a", "Partridge in a Pear Tree"],
+          ["second", "two", "Turtle Doves"],
+          ["third", "three", "French Hens"],
+          ["fourth", "four", "Calling Birds"],
+          ["fifth", "five", "Gold Rings"],
+          ["sixth", "six", "Geese-a-Laying"],
+          ["seventh", "seven", "Swans-a-Swimming"],
+          ["eighth", "eight", "Maids-a-Milking"],
+          ["ninth", "nine", "Ladies Dancing"],
+          ["tenth", "ten", "Lords-a-Leaping"],
+          ["eleventh", "eleven", "Pipers Piping"],
+          ["twelfth", "twelve", "Drummers Drumming"]
+  ]
+  def self.song
+    lyrics = ""
+    accumulated = ""
+    TRUE_LOVE.each do |month|
+      if !lyrics.empty?
+        lyrics += "\n"
+      end
+      this_month = "#{month[1]} #{month[2]}"
+      lyrics += "On the #{month[0]} day of Christmas my true love gave to me: #{this_month}#{accumulated}.\n"
+      connector = ", "
+      if accumulated.empty?
+        connector += "and "
+      end
+      accumulated = "#{connector}#{this_month}#{accumulated}"
+    end
+    lyrics
+  end
+end
