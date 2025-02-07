ddd32eeece9ca0b58148797b66a3d362adeec2be
David Gomez G. <dgomezg@gmail.com>
2025-02-07 19:59:54 +0100

    Solved Exercism/space-age

--- exercism/space-age/space_age.rb
+++ exercism/space-age/space_age.rb
@@ -1,7 +1,17 @@
-=begin
-Write your code for the 'Space Age' exercise in this file. Make the tests in
-`space_age_test.rb` pass.
+class SpaceAge
+  SECONDS_PER_YEAR = (365.25*24*60*60)
+  attr_reader :on_earth, :on_mercury, :on_venus, :on_mars,
+              :on_jupiter, :on_saturn, :on_uranus, :on_neptune
 
-To get started with TDD, see the `README.md` file in your
-`ruby/space-age` directory.
-=end
+  def initialize(seconds)
+    @on_earth = seconds / SECONDS_PER_YEAR
+    @on_mercury = @on_earth / 0.2408467
+    @on_venus = @on_earth / 0.61519726
+    @on_mars = @on_earth / 1.8808158
+    @on_jupiter = @on_earth / 11.862615
+    @on_saturn = @on_earth / 29.447498
+    @on_uranus = @on_earth / 84.016846
+    @on_neptune = @on_earth / 164.79132
+  end
+
+end
