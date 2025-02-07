711068114f6cf9ef38d475dbcaa5322e53a0c21f
David Gomez G. <dgomezg@gmail.com>
2025-02-07 19:18:57 +0100

    Second version of Exercism/rna-transcription based on community solutions

--- exercism/rna-transcription/rna_transcription.rb
+++ exercism/rna-transcription/rna_transcription.rb
@@ -1,14 +1,15 @@
 module Complement
 
   def self.of_dna(dna)
-    dna.each_char.map  do |c|
-      case c
-      when 'G' then 'C'
-      when 'C' then 'G'
-      when 'T' then 'A'
-      when 'A' then 'U'
-      else ''
-      end
-    end.join
+    dna.tr('GCTA', 'CGAU')
+#    dna.each_char.map  do |c|
+#      case c
+#      when 'G' then 'C'
+#      when 'C' then 'G'
+#      when 'T' then 'A'
+#      when 'A' then 'U'
+#      else ''
+#      end
+#    end.join
   end
 end
