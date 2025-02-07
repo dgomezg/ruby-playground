90dfcb7193c6280031d1dbee8bafe9b87b0d1417
David Gomez G. <dgomezg@gmail.com>
2025-02-07 19:15:48 +0100

    Solved Exercism/rna-transcription

--- exercism/rna-transcription/rna_transcription.rb
+++ exercism/rna-transcription/rna_transcription.rb
@@ -1,7 +1,14 @@
-=begin
-Write your code for the 'Rna Transcription' exercise in this file. Make the tests in
-`rna_transcription_test.rb` pass.
+module Complement
 
-To get started with TDD, see the `README.md` file in your
-`ruby/rna-transcription` directory.
-=end
+  def self.of_dna(dna)
+    dna.each_char.map  do |c|
+      case c
+      when 'G' then 'C'
+      when 'C' then 'G'
+      when 'T' then 'A'
+      when 'A' then 'U'
+      else ''
+      end
+    end.join
+  end
+end
