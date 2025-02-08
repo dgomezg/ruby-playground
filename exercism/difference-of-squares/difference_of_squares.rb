7141a5cd59c92731f6dab451caf015fa35b1b4cf
David Gomez G. <dgomezg@gmail.com>
2025-02-08 13:29:08 +0100

    Improved Exercism/difference_of_squares

--- exercism/difference-of-squares/difference_of_squares.rb
+++ exercism/difference-of-squares/difference_of_squares.rb
@@ -2,12 +2,15 @@ class Squares
   def initialize(up_to)
     @up_to = up_to
   end
+
   def square_of_sum
-    (1..@up_to).reduce(:+)**2
+    #(1..@up_to).reduce(:+) ** 2
+    (1..@up_to).sum ** 2
   end
 
   def sum_of_squares
-    (1..@up_to).map{|n | n**2}.reduce(:+)
+    #(1..@upto).map { |n| n ** 2 }.reduce(:+)
+    (1..@up_to).sum { |n| n ** 2 }
   end
 
   def difference
