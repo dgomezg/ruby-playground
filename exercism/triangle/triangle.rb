ea05cb11c879593b20f4cdb78707e1ea0511608c
David Gomez G. <dgomezg@gmail.com>
2025-02-08 12:00:07 +0100

    Solved Exercism/triangle practice

--- exercism/triangle/triangle.rb
+++ exercism/triangle/triangle.rb
@@ -1,7 +1,27 @@
-=begin
-Write your code for the 'Triangle' exercise in this file. Make the tests in
-`triangle_test.rb` pass.
+class Triangle
 
-To get started with TDD, see the `README.md` file in your
-`ruby/triangle` directory.
-=end
+  def initialize(sides)
+    raise ArgumentError("wrong number of sides") unless sides.size == 3
+    @sides = sides
+  end
+
+  def equilateral?
+    is_a_triangle? && @sides.uniq.size == 1
+  end
+  def isosceles?
+    is_a_triangle? && @sides.uniq.size <= 2
+  end
+
+  def scalene?
+    is_a_triangle? && @sides.uniq.size == 3
+  end
+
+  private
+  def is_a_triangle?
+    @sides.reject {|a| a <= 0}.size == 3 &&
+      @sides[0] + @sides[1] >= @sides[2] &&
+      @sides[1] + @sides[2] >= @sides[0] &&
+      @sides[2] + @sides[0] >= @sides[1]
+  end
+
+end
