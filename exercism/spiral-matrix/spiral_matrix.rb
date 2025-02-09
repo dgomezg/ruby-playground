4187fedf1a5abe691298fed24d44148ae9353226
David Gomez G. <dgomezg@gmail.com>
2025-02-09 20:23:22 +0100

    Solved Exercism/spiral-matrix practice

--- exercism/spiral-matrix/spiral_matrix.rb
+++ exercism/spiral-matrix/spiral_matrix.rb
@@ -1,7 +1,30 @@
-=begin
-Write your code for the 'Spiral Matrix' exercise in this file. Make the tests in
-`spiraL_matrix_test.rb` pass.
+class SpiralMatrix
+  attr_reader :matrix
 
-To get started with TDD, see the `README.md` file in your
-`ruby/spiral-matrix` directory.
-=end
+  def initialize(matrix_size)
+    @matrix = Array.new(matrix_size) { Array.new(matrix_size, 0) }
+    count = 1
+    row = column = 0
+    directions = [:right, :down, :left, :up]
+    while count <= matrix_size ** 2
+      @matrix[row][column] = count
+
+      if (directions[0] == :right && (column +1 == matrix_size || @matrix[row][column+1] != 0)) ||
+         (directions[0] == :down  && (row +1 == matrix_size || @matrix[row+1][column] != 0)) ||
+         (directions[0] == :left  && (column == 0 || @matrix[row][column -1] != 0)) ||
+         (directions[0] == :up    && (row == 0 || @matrix[row-1][column] != 0))
+        directions.rotate!(1)
+      end
+
+      case directions[0]
+        when :right then column += 1
+        when :down  then row += 1
+        when :left  then column -= 1
+        when :up    then row -= 1
+      end
+
+      count += 1
+    end
+  end
+
+end
