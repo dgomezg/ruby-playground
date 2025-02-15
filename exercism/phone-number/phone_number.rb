ce4ea398d740687555717a7335c81e19b043a6de
David Gomez G. <dgomezg@gmail.com>
2025-02-15 11:03:27 +0100

    Solved Exercism/phone-number practice

--- exercism/phone-number/phone_number.rb
+++ exercism/phone-number/phone_number.rb
@@ -1,7 +1,17 @@
-=begin
-Write your code for the 'Phone Number' exercise in this file. Make the tests in
-`phone_number_test.rb` pass.
+module PhoneNumber
 
-To get started with TDD, see the `README.md` file in your
-`ruby/phone-number` directory.
-=end
+  NANP_FORMAT = %r{
+    \A
+    \+?(?<internationl_code>1?)
+    \D*(?<area_code>[2-9][0-9]{2})
+    \D*(?<exchange_code>[2-9][0-9]{2})
+    \D*(?<subscriber_number>[0-9]{4})
+    \D*
+    \z
+  }x
+
+  def self.clean(phone_number)
+    result = phone_number.match(NANP_FORMAT)
+    result.nil? ? nil : result.captures[1..-1].join
+  end
+end
